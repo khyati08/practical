e7fed1d7cb7e3cd711579ffd16d01384a471883c
Khyati-Maktech <khyati@maktech.info>
2022-05-05 16:41:02 +0530

    # Practical Code

--- app/src/androidTest/java/com/khyati/practical/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/khyati/practical/ExampleInstrumentedTest.kt
@@ -1,9 +1,9 @@
 package com.khyati.practical
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import com.khyati.practical.viewmodel.FirstViewModel
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 /**
@@ -13,10 +13,24 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule(MainActivity::class.java, true, true)
+
+    private lateinit var viewModel: FirstViewModel
+/*
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.khyati.practical", appContext.packageName)
+    }*/
+
+    @Before
+    fun setUp() {
+        // Mock our View Model to stub out calls later
+        viewModel = mock()
+
     }
 }
